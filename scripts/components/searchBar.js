63e51f3fdad2e34ce6bf67fc83104b8d11493369
tamoroso <amorosothomas@orange.fr>
2022-09-08 17:03:10 +0200

    Removing array method form handleSearch function

--- scripts/components/searchBar.js
+++ scripts/components/searchBar.js
@@ -1,34 +1,54 @@
 const recipesData = JSON.parse(window.localStorage.getItem("recipes"));
 
 const handleSearch = (recipes, searchParams) => {
-  return recipes.reduce((acc, recipe) => {
-    if (
-      (searchParams.length === 1 && searchParams[0].length < 3) ||
-      searchParams.length <= 0
-    ) {
-      acc.push(recipe);
-    } else {
-      const ingredientList = getIngredientList(recipe.ingredients);
-      const searchableProps = [
-        recipe.name,
-        ...ingredientList,
-        recipe.description,
-      ];
-      const searchResult =
-        searchableProps
-          .map((el) => (el ? normalizeString(el.toLowerCase()) : ""))
-          .filter((el) =>
-            searchParams.some((param) => el.includes(normalizeString(param)))
-          ).length >= searchParams.length;
-      if (searchResult) {
-        acc.push(recipe);
+  let res = [];
+  if (
+    (searchParams.length === 1 && searchParams[0].length < 3) ||
+    searchParams.length <= 0
+  ) {
+    return recipes;
+  } else {
+    for (let param = 0; param < searchParams.length; param++) {
+      for (let i = 0; i < recipes.length; i++) {
+        const recipeName = recipes[i].name;
+        const ingredientList = getIngredientList(recipes[i].ingredients);
+        const recipeDescription = recipes[i].description;
+        const searchableProps = [
+          recipeName,
+          ...ingredientList,
+          recipeDescription,
+        ];
+        for (
+          let propIndex = 0;
+          propIndex < searchableProps.length;
+          propIndex++
+        ) {
+          const currentProp = normalizeString(
+            searchableProps[propIndex].toLowerCase()
+          );
+          if (currentProp.includes(searchParams[param])) {
+            res.push(recipes[i]);
+            break;
+          }
+        }
       }
     }
-    return acc;
-  }, []);
+    return res;
+  }
 };
 
 const searchBar = document.querySelector("#search");
+
+const stringToArray = (string) => {
+  if (string.trim().length === 0) {
+    return [];
+  }
+  let array = string.split(" ");
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i].toString().toLowerCase();
+  }
+  return array;
+};
 
 searchBar.addEventListener("input", (event) => {
   const searchParams = event.target.value
